8abc633e64520b038ceb6ef43d407758e1c2685e
Sameer Kumar <sameer.cusat2019@gmail.com>
2026-04-28 07:31:25 +0530

    DataIngestion Completed

---
+++ EtoEMLProject/src/components/data_ingestion.py
@@ -0,0 +1,40 @@
+import sys
+import os
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path:str = os.path.join('artifacts','train.csv')
+    test_data_path:str = os.path.join('artifacts','test.csv')
+    raw_data_path:str = os.path.join('artifacts','raw_data.csv')
+
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig()
+    
+    def initiate_data_ingestion(self):
+        logging.info('Data Ingestion Started')
+        try:
+            df=pd.read_csv(r'C:\ML\EtoEMLProject\notebook\stud.csv')
+            logging.info('Read the dataset as dataframe')
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+            logging.info('Train Test Split Initiated')
+            train_data,test_data = train_test_split(df,test_size=0.3,random_state=42)
+            train_data.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_data.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            logging.info('Ingestion of Data is Completed')
+            return(
+                self.ingestion_config.train_data_path,self.ingestion_config.test_data_path
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
+
+if __name__ == '__main__':
+    data_ingestion=DataIngestion()
+    data_ingestion.initiate_data_ingestion()
